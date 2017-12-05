9bf04e6fb115aca96379207e2c8227da43b3453a
Brian Prescott <bp@wave39.com>
2017-12-04 20:35:49 -0500

    Reorganize main class

--- AdventOfCode2017/main.swift
+++ AdventOfCode2017/main.swift
@@ -11,39 +11,44 @@ import Foundation
 print ("")
 print ("Welcome to BP's Advent Of Code 2017 puzzle solver.")
 print ("Make sure to click in the Output window to enter which puzzle you would like to solve.")
-print ("")
 
 let defaultPuzzle = 4
 
-var puzzle = 0
-while (puzzle < 1 || puzzle > 25) {
-    print ("Which puzzle would you like to solve? (Enter a number from 1 to 25, default of \(defaultPuzzle)")
-    let response = readLine()
-    if (response != "") {
-        puzzle = Int(response ?? "0")!
-    } else {
-        puzzle = defaultPuzzle
-        print ("Defaulting to puzzle \(defaultPuzzle)")
+var quitApp = false
+while !quitApp {
+    var puzzle = 0
+    while !quitApp && (puzzle < 1 || puzzle > 25) {
+        print ("")
+        print ("Which puzzle would you like to solve? (Enter a number from 1 to 25, default of \(defaultPuzzle), or Q to quit)")
+        let response = readLine()
+        if response == "q" || response == "Q" {
+            quitApp = true
+        } else if response != "" {
+            puzzle = Int(response ?? "0")!
+        } else {
+            puzzle = defaultPuzzle
+            print ("Defaulting to puzzle \(defaultPuzzle)")
+        }
+    }
+
+    if !quitApp {
+        print ("")
+        print ("Solving puzzle \(puzzle), please stand by...")
+
+        if puzzle == 1 {
+            let d = Day01()
+            d.solve()
+        } else if puzzle == 2 {
+            let d = Day02()
+            d.solve()
+        } else if puzzle == 3 {
+            Day03().solve()
+        } else if puzzle == 4 {
+            Day04().solve()
+        }
     }
 }
 
 print ("")
-print ("Solving puzzle \(puzzle), please stand by...")
-
-if (puzzle == 1) {
-    let d = Day01()
-    d.solve()
-} else if (puzzle == 2) {
-    let d = Day02()
-    d.solve()
-} else if (puzzle == 3) {
-    let d = Day03()
-    d.solve()
-} else if (puzzle == 4) {
-    let d = Day04()
-    d.solve()
-}
-
-print ("")
 print ("Press Enter to end the puzzle solving session")
-let endOfLine = readLine()
+let _ = readLine()
